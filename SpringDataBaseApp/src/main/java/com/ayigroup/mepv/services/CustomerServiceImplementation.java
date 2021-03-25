2d1bc81548305e13a28b6ae8c45b03d274030111
mepv <mepvdevelopments@gmail.com>
2021-03-25 15:24:43 -0300

    añadiendo comentarios en la implementación de los servicios

--- SpringDataBaseApp/src/main/java/com/ayigroup/mepv/services/CustomerServiceImplementation.java
+++ SpringDataBaseApp/src/main/java/com/ayigroup/mepv/services/CustomerServiceImplementation.java
@@ -14,22 +14,49 @@ public class CustomerServiceImplementation implements CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
+    /**
+     * Display all the Customers from the database.
+     *
+     * @return a {@link java.util.List List<>} of customers.
+     */
     @Override
     public List<Customer> getCustomers() {
         return customerRepository.findAll();
     }
 
+    /**
+     * Save a Customer to the database.
+     *
+     * @param customer the customer object.
+     */
     @Override
     public void saveCustomer(Customer customer) {
         this.customerRepository.save(customer);
     }
 
+    /**
+     * Overload method to be implemented by the RestController class.
+     * Save a Customer to the database.
+     *
+     * @param firstName the first name.
+     * @param lastName the last name.
+     * @param email the email.
+     */
     @Override
     public void saveCustomer(String firstName, String lastName, String email) {
         Customer tempCustomer = new Customer(firstName, lastName, email);
         saveCustomer(tempCustomer);
     }
 
+    /**
+     * Retrieve a specific Customer from the database.
+     *
+     * @param id the id of the customer.
+     *
+     * @throws RuntimeException in case it was not found.
+     *
+     * @return the customer object.
+     */
     @Override
     public Customer getCustomerById(long id) {
         Optional<Customer> optional = customerRepository.findById(id);
@@ -42,6 +69,11 @@ public class CustomerServiceImplementation implements CustomerService {
         return customer;
     }
 
+    /**
+     * Delete a Customer from the database.
+     *
+     * @param id the id of the customer.
+     */
     @Override
     public void deleteCustomerById(long id) {
         this.customerRepository.deleteById(id);
